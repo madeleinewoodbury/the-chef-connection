b0e20c03dfe99c381a726199acb4ff49800e9b62
Madeleine Woodbury <randine25@gmail.com>
2019-05-23 12:31:22 -0400

    Login form updated with useSate hooks

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,65 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  // Update form data
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault(e);
+    console.log('Success');
+  };
+
+  return (
+    <Fragment>
+      <div className="form-container">
+        <h1 className="large text-primary text-center">Sign In</h1>
+        <p className="lead text-center">
+          <i className="fas fa-user" /> Sign in to your account
+        </p>
+
+        <form className="form" onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={e => onChange(e)}
+              required
+              minLength="6"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Login"
+            className="btn btn-primary btn-block"
+          />
+        </form>
+        <p className="my-1 text-center">
+          Don't have an account? <Link to="/register">Sign Up</Link>
+        </p>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Login;
