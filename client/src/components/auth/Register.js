9d235d09f5dd5400d04ab85cd4bd0021d23bbcce
Madeleine Woodbury <randine25@gmail.com>
2019-05-23 12:19:09 -0400

    Register form updated with useState hooks

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,92 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault(e);
+
+    // Check if passwords match
+    if (password !== password2) {
+      console.log('Passwords do not match');
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <Fragment>
+      <div className="form-container">
+        <h1 className="large text-primary text-center">Sign Up</h1>
+        <p className="lead text-center">
+          <i className="fas fa-user" /> Create Your Account
+        </p>
+
+        <form className="form" onSubmit={e => onSubmit(e)}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={e => onChange(e)}
+              required
+              minLength="6"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              minLength="6"
+              name="password2"
+              value={password2}
+              onChange={e => onChange(e)}
+              required
+            />
+          </div>
+          <input
+            type="submit"
+            value="Register"
+            className="btn btn-primary btn-block"
+          />
+        </form>
+        <p className="my-1 text-center">
+          Already have an account? <a href="login.html">Sign In</a>
+        </p>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Register;
